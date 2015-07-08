ce5673a5ebfb6dffa7a6e8a2b956ad385c38dc1c
Vincent van 't Westende <vincent@zimmermanzimmerman.nl>
2015-07-08 15:41:08 +0200

    2 layer heatmap

--- js/main.js
+++ js/main.js
@@ -23,21 +23,20 @@ function OipaMap(){
             zoom: 3,
             minZoom: 2,
             maxZoom:12,
-            continuousWorld: 'false'
+            continuousWorld: 'false',
+            zoomControl: 'true'
         }
 
         if(no_dragging){
             mapoptions.dragging = false;
         }
 
-        if(zoomposition || zoomposition == null){
-            mapoptions.zoomControl = false;
-        }
+        // if(zoomposition || zoomposition == null){
+        //     mapoptions.zoomControl = false;
+        // }
 
-
-
-        jQuery("#"+div_id).css("min-height", "400px");
-        this.map = L.map('heatmap', mapoptions).setView([3.505, 18.00], 2);
+        jQuery("#"+div_id).css("min-height", "450px");
+        this.map = L.map('heatmap', mapoptions).setView([-1.292066, 36.821946], 11);
 
         if (zoomposition){
             new L.Control.Zoom({ position: zoomposition }).addTo(this.map);
@@ -46,25 +45,47 @@ function OipaMap(){
         this.tl = L.tileLayer('https://{s}.tiles.mapbox.com/v3/'+this.basemap+'/{z}/{x}/{y}.png', {
             maxZoom: 12
         }).addTo(this.map);
+
+        this.heat = [];
+
+        this.heat[0] = L.heatLayer([], {
+            radius: 25,
+            blur: 15,
+            gradient: {0.4: 'yellow', 0.65: 'lime', 1: 'red'}
+        }).addTo(this.map);
+
+        this.heat[1] = L.heatLayer([], {
+            radius: 25,
+            blur: 15,
+            gradient: {0.4: '#55acee', 0.65: '#4fbdbe', 1: '#147bc9'}
+        }).addTo(this.map);
     };
 
     this.refresh = function(data){
-
-        if (!data){
-            // get url
-            var url = this.get_url();
-            // get data
-            this.get_data(url);
-        } else {
-            // show data
-            this.show_data_on_map(data);
+        if(data){
+            this.reDraw(data);
+        } else{
+            this.getData();
         }
+
     };
 
 
-    this.get_data = function(url){
-        return dollyData;
+    this.getData = function(url){
+        this.refresh(dollyData);
     };
+
+    this.reDraw = function(data){
+
+        for (var i = 0;i< 500;i++){
+            this.heat[0].addLatLng([data[i]['fields']['latitude'], data[i]['fields']['longtitude']]);
+        }
+
+        for (var i = 500;i< data.length;i++){
+            this.heat[1].addLatLng([data[i]['fields']['latitude'], data[i]['fields']['longtitude']]);
+        }
+        
+    }
 
 }
 
@@ -72,4 +93,5 @@ function OipaMap(){
 var dollymap = new OipaMap();
 
 dollymap.set_map('heatmap');
-console.log('test');
+dollymap.refresh();
+console.log(dollyData);
